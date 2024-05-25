bd3177a5093231b0e60e6af51e3c2e34f389cc30
harimurugan1989 <harimur@gmail.com>
2024-05-25 15:40:36 +0530

    news section udpated

--- src/components/AboutNews.jsx
+++ src/components/AboutNews.jsx
@@ -79,21 +79,34 @@ function AboutNews(props) {
           // className="flex flex-col  animate-wiggle"
           >
 
-            {newsData.map((item) => (
-              <Link key={item._id} to={`/news/${item._id}`}>
 
-                <div className="relative my-4 rounded-xl hover:shadow-lg hover:shadow-accent-600/50 bg-white border-solid border-[3px] border-accent-500">
+                      {newsData.map((item) => (
+                          <Link key={item._id} target="_blank" rel="noopener noreferrer"
+                              to={item.link !== "" ? item.link : `/news/${item._id}`}  >
+
+                              {!item.hidden && <div className="relative my-4 rounded-xl hover:shadow-lg hover:shadow-accent-600/50 bg-white border-solid border-[3px] border-accent-500">
                   <div className="w-2 h-2 animate-ping bg-accent-600 absolute  -left-1 -top-1 rounded-full"></div>
                   <p className="text-lg font-medium  text-accent-600 p-4 -mt-2 border-b-2 border-dashed border-accent-400">{item.title}</p>
-                  <p className="text-sm pt-4 p-4">
+
+                                      {item.new && <span className=' absolute mr-10 right-0 mt-5 flex flex-row animate-bounce '>
+
+                                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="w-5 stroke-red-400 ">
+                                              <path strokeLinecap="round" strokeLinejoin="round" d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z" />
+                                          </svg>
+                                          <p className=" text-red-400 text-sm ml-1 font-semibold">NEW</p>
+                                      </span >}
+
+                                 
+                     <p className="text-sm pt-4 p-4">
                     {item.metaDescription}
                   </p>
+                              </div>
+                            
+                              }
+                          </Link>
+                      ))}
 
-
-                </div>
-              </Link>
-
-            ))}
+       
           </div>
         </div>
       </div>
