320116f896d9ffe5a2c98d2f4e00be4bc73e01b5
harimurugan1989 <harimur@gmail.com>
2024-05-29 08:29:04 +0530

    redirects updated

--- src/pages/Papers.jsx
+++ src/pages/Papers.jsx
@@ -35,7 +35,7 @@ useEffect(() => {
         <div className="container max-w-7xl mx-auto px-5 sm:px-10  lg:px-8 mt-[58px] min-h-[300px] lg:mt-[70px] mb-5">
                 <div className="w-full mx-auto  md:w-[700px] px-4 lg:w-full" >
                     <p className="text-2xl font-sans font-bold mb-5  text-gray-950 underline ">
-                        {data? "Tracks for Paper Submission": "Paper Tracks"}
+                        {data? "Tracks for Paper Submission": "Loading.."}
                     </p>
                     <p className="text-base  text-justify font-sans font-base text-gray-800">
                       
