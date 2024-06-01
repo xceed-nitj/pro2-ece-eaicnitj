985228df020d62eb26048957e7dfbe93faaefcea
Aditya kumar <98655260+164adityakumar@users.noreply.github.com>
2024-06-02 02:06:13 +0530

    Added image slider in mobile view , added hero section fillers

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -327,7 +327,7 @@ function HeroSection() {
   }, [currentIndex, nextSlide]);
   return (
     <>
-      <div className="Hero flex md:flex-row flex-col  bg-accent-300 sm:mt-[52px] mt-[40px] ">
+      <div className="Hero pattern flex md:flex-row flex-col  sm:mt-[52px] mt-[40px] ">
         <div className="container lg:max-w-7xl p-4  mx-auto md:mb-5 flex flex-col xl:gap-2 xl:mt-6 xl:ml-10 xl:-mr-14 xl:pr-0 ">
           <div className='flex flex-col justify-center'>
           <div className="ml-8 flex flex-row gap-2 mb-3 md:text-left text-center justify-center md:justify-normal">
