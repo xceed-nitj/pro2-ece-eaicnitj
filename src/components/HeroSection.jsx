a6be5e8100950b0036cb024499d55322a8670c4c
harimurugan1989 <harimur@gmail.com>
2024-12-06 17:38:46 +0530

    home update

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -346,10 +346,10 @@ function HeroSection() {
           </div>
           </div>
           <h1 className="xl:text-5xl md:text-4.5xl text-4xl md:ml-8 mt-1.5 font-bold sm:text-left text-center text-accent-900 2xl:text-6xl">
-            IEEE sponsored International Conference <br />
-            <span className="ON md:text-3xl xl:text-4xl text-3xl text-center text-accent-900 xl:mt-4 ">
-              on
-            </span>
+                      IEEE sponsored <br /> International Conference on 
+            {/*<span className="ON md:text-3xl xl:text-4xl text-3xl text-center text-accent-900 xl:mt-4 ">*/}
+              
+            {/*</span>*/}
             <br />
             <span className="md:text-5xl xl:text-6xl text-4xl font-bold text-justify text-accent-950 xl:mt-4 2xl:text-7xl">
               Electronics, AI and Computing
@@ -366,7 +366,7 @@ function HeroSection() {
 
 
           
-          <div className='md:mt-4 mt-10 flex md:flex-row flex-col md:gap-0  rounded-full sm:justify-between md:ml-7 xl:justify-start 2xl:pt-4 justify-center gap-11'>
+          <div className='md:mt-4 mt-10 flex md:flex-row flex-col md:gap-0  rounded-full sm:justify-between md:ml-7 xl:justify-start 2xl:pt-4 justify-center gap-22'>
             <div className='md:mr-3 flex flex-col justify-center'>           
               <a
               href="#"
@@ -376,7 +376,7 @@ function HeroSection() {
               <span className="text-xs bg-accent-600 rounded-full text-white px-1.5 py-2 mr-3 font-medium 2xl:px-[0.8rem] 2xl:py-4 ">
               <svg xmlns="http://www.w3.org/2000/svg" height="18" viewBox="0 -960 960 960" width="22"><path d="M80-160v-112q0-33 17-62t47-44q51-26 115-44t141-18q30 0 58.5 3t55.5 9l-70 70q-11-2-21.5-2H400q-71 0-127.5 17T180-306q-9 5-14.5 14t-5.5 20v32h250l80 80H80Zm542 16L484-282l56-56 82 82 202-202 56 56-258 258ZM400-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47Zm10 240Zm-10-320q33 0 56.5-23.5T480-640q0-33-23.5-56.5T400-720q-33 0-56.5 23.5T320-640q0 33 23.5 56.5T400-560Zm0-80Z" className=' fill-accent-50 2xl:h-24'/></svg>
               </span>
-              <span className="text-sm font-semibold 2xl:text-lg">
+              <span className="text-sm font-semibold 2xl:text-lg ">
                 Register
               </span>
               <svg
