81d8126fdc805c113df64bf8535db361389528c0
harimurugan1989 <harimur@gmail.com>
2025-08-01 15:49:25 +0530

    acknow

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -100,7 +100,7 @@ function Home(props) {
           <OrganizingHeads />
         </div>
       </section>
-      
+      <p> The Microsoft CMT service was used for managing the peer-reviewing process for this conference. This service was provided for free by Microsoft and they bore all expenses, including costs for Azure cloud services as well as for software development and support.</p>
       {/* Footer */}
       <Footer />
     </div>
